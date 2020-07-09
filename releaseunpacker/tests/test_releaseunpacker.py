2f47ee3e906959b5465c15bf2e42acd7c8d90490
dnxxx <dnx@fbi-security.net>
2020-07-09 17:44:45 +0200

    test_releaseunpacker.py: Update tests

--- releaseunpacker/tests/test_releaseunpacker.py
+++ releaseunpacker/tests/test_releaseunpacker.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import unittest
 from tempfile import mkdtemp
+from unittest import mock
 
 from unipath import Path
 
@@ -14,64 +15,96 @@ from releaseunpacker.releaseunpacker import (
     ReleaseUnpackerRarFileError,
 )
 
-TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_FILES = Path(os.path.dirname(os.path.abspath(__file__)), "files")
 
 
-class TestReleaseUnpacker(unittest.TestCase):
-    """ReleaseUnpacker test case."""
+class ReleaseUnpackerTestCase:
+    """ReleaseUnpacker test case base class."""
 
     def setUp(self):
         """Test setup."""
+        self.search_dir = mkdtemp()
         self.tmp_dir = mkdtemp()
         self.unpack_dir = mkdtemp()
 
     def tearDown(self):
         """Test cleanup."""
+        shutil.rmtree(self.search_dir)
         shutil.rmtree(self.tmp_dir)
         shutil.rmtree(self.unpack_dir)
 
+    def copy_test_file_to_search_dir(self, file_name):
+        """Copy test file to search dir."""
+        return TEST_FILES.child(file_name).copy(
+            Path(self.search_dir, file_name)
+        )
+
+    def copy_test_directory_to_search_dir(self, source):
+        """Copy test directory to search dir."""
+        return distutils.dir_util.copy_tree(
+            Path(TEST_FILES, source), Path(self.search_dir, source)
+        )
+
+    def copy_test_file_to_tmp_dir(self, file_name):
+        """Copy test file to tmp dir."""
+        return TEST_FILES.child(file_name).copy(Path(self.tmp_dir, file_name))
+
+    def copy_test_file_to_unpack_dir(self, file_name):
+        """Copy test file to unpack dir."""
+        return TEST_FILES.child(file_name).copy(
+            Path(self.unpack_dir, file_name)
+        )
+
+
+class TestReleaseUnpacker(ReleaseUnpackerTestCase, unittest.TestCase):
+    """ReleaseUnpacker test case."""
+
+    LOGGER_NAME = "releaseunpacker.releaseunpacker"
+
     def test_repr(self):
         """Test object string representation."""
-        release_search_dir = Path(TEST_DIR, "files/")
-        ru = ReleaseUnpacker(release_search_dir, self.tmp_dir, self.unpack_dir)
+        release_unpacker = ReleaseUnpacker(
+            self.search_dir, self.tmp_dir, self.unpack_dir
+        )
+
         self.assertEqual(
-            ru.__repr__(),
+            release_unpacker.__repr__(),
             "<ReleaseUnpacker: {} ({}) ({})>".format(
-                release_search_dir.absolute(), self.tmp_dir, self.unpack_dir
+                self.search_dir, self.tmp_dir, self.unpack_dir
             ),
         )
 
     def test_release_search_dir_does_not_exist(self):
         """Test non existing search dir raises exception."""
-        release_search_dir = Path("/non_existing_release_search_dir")
+        search_dir = Path("/non_existing_release_search_dir")
 
         with self.assertRaises(ReleaseUnpackerError) as cm:
-            ReleaseUnpacker(release_search_dir, self.tmp_dir, self.unpack_dir)
+            ReleaseUnpacker(search_dir, self.tmp_dir, self.unpack_dir)
 
         self.assertEqual(
             str(cm.exception),
-            "Release search dir {} doesn't exist".format(release_search_dir),
+            "Release search dir {} doesn't exist".format(search_dir),
         )
 
     def test_release_search_dir_not_a_dir(self):
         """Test release search dir is not a directory raises exception."""
-        release_search_dir = Path(TEST_DIR, "files/file_not_a_dir")
+        self.copy_test_file_to_search_dir("file_not_a_dir")
+        search_dir = Path(self.search_dir, "file_not_a_dir")
 
         with self.assertRaises(ReleaseUnpackerError) as cm:
-            ReleaseUnpacker(release_search_dir, self.tmp_dir, self.unpack_dir)
+            ReleaseUnpacker(search_dir, self.tmp_dir, self.unpack_dir)
 
         self.assertEqual(
             str(cm.exception),
-            "Release search dir {} is not a dir".format(release_search_dir),
+            "Release search dir {} is not a dir".format(search_dir),
         )
 
     def test_tmp_dir_does_not_exist(self):
         """Test non existing tmp dir raises exception."""
-        release_search_dir = Path(TEST_DIR, "files/")
         tmp_dir = Path("/non_existing_tmp_dir")
 
         with self.assertRaises(ReleaseUnpackerError) as cm:
-            ReleaseUnpacker(release_search_dir, tmp_dir, self.unpack_dir)
+            ReleaseUnpacker(self.search_dir, tmp_dir, self.unpack_dir)
 
         self.assertEqual(
             str(cm.exception), "Tmp dir {} doesn't exist".format(tmp_dir)
@@ -79,11 +112,11 @@ class TestReleaseUnpacker(unittest.TestCase):
 
     def test_tmp_dir_not_a_dir(self):
         """Test tmp dir is not a directory raises exception."""
-        release_search_dir = Path(TEST_DIR, "files/")
-        tmp_dir = Path(TEST_DIR, "files/file_not_a_dir")
+        self.copy_test_file_to_tmp_dir("file_not_a_dir")
+        tmp_dir = Path(self.tmp_dir, "file_not_a_dir")
 
         with self.assertRaises(ReleaseUnpackerError) as cm:
-            ReleaseUnpacker(release_search_dir, tmp_dir, self.unpack_dir)
+            ReleaseUnpacker(self.search_dir, tmp_dir, self.unpack_dir)
 
         self.assertEqual(
             str(cm.exception), "Tmp dir {} is not a dir".format(tmp_dir)
@@ -91,11 +124,10 @@ class TestReleaseUnpacker(unittest.TestCase):
 
     def test_unpack_dir_does_not_exist(self):
         """Test non existing unpack dir raises exception."""
-        release_search_dir = Path(TEST_DIR, "files/")
         unpack_dir = Path("/non_existing_unpack_dir")
 
         with self.assertRaises(ReleaseUnpackerError) as cm:
-            ReleaseUnpacker(release_search_dir, self.tmp_dir, unpack_dir)
+            ReleaseUnpacker(self.search_dir, self.tmp_dir, unpack_dir)
 
         self.assertEqual(
             str(cm.exception), "Unpack dir {} doesn't exist".format(unpack_dir)
@@ -103,75 +135,70 @@ class TestReleaseUnpacker(unittest.TestCase):
 
     def test_unpack_dir_not_a_dir(self):
         """Test unpack dir is not a directory raises exception."""
-        release_search_dir = Path(TEST_DIR, "files/")
-        unpack_dir = Path(TEST_DIR, "files/file_not_a_dir")
+        self.copy_test_file_to_unpack_dir("file_not_a_dir")
+        unpack_dir = Path(self.unpack_dir, "file_not_a_dir")
 
         with self.assertRaises(ReleaseUnpackerError) as cm:
-            ReleaseUnpacker(release_search_dir, self.tmp_dir, unpack_dir)
+            ReleaseUnpacker(self.search_dir, self.tmp_dir, unpack_dir)
 
         self.assertEqual(
             str(cm.exception), "Unpack dir {} is not a dir".format(unpack_dir)
         )
 
-    def test_scan_rars(self):
-        """Test scanning of RAR files."""
-        release_search_dir = Path(TEST_DIR, "files/")
-        ru = ReleaseUnpacker(release_search_dir, self.tmp_dir, self.unpack_dir)
+    def test_scan_rars_single_release(self):
+        """Test scanning of RAR files for a single release."""
+        self.copy_test_directory_to_search_dir("Release-Group")
+        release_unpacker = ReleaseUnpacker(
+            self.search_dir, self.tmp_dir, self.unpack_dir
+        )
+
         self.assertEqual(
-            ru.scan_rars(),
+            release_unpacker.scan_rars(),
+            [Path(self.search_dir, "Release-Group/rar_file.rar"),],
+        )
+
+    def test_scan_rars_multiple_releases(self):
+        """Test scanning of RAR files with multiple releases in search dir."""
+        for release in ("Release-Group", "Release.with.subs-Group"):
+            self.copy_test_directory_to_search_dir(release)
+
+        release_unpacker = ReleaseUnpacker(
+            self.search_dir, self.tmp_dir, self.unpack_dir
+        )
+
+        self.assertEqual(
+            release_unpacker.scan_rars(),
             [
-                Path(TEST_DIR, "files/Release-Group/rar_file.rar"),
+                Path(self.search_dir, "Release-Group/rar_file.rar"),
                 Path(
-                    TEST_DIR,
-                    (
-                        "files/Release.with.subs-Group/"
-                        "release.with.subs-group.rar"
-                    ),
+                    self.search_dir,
+                    "Release.with.subs-Group/release.with.subs-group.rar",
                 ),
                 Path(
-                    TEST_DIR,
-                    ("files/Release.with.subs-Group/Subs" "/subs.rar"),
+                    self.search_dir, "Release.with.subs-Group/Subs/subs.rar",
                 ),
             ],
         )
 
-        release_search_dir = Path(TEST_DIR, "files/Release-Group")
-        ru = ReleaseUnpacker(release_search_dir, self.tmp_dir, self.unpack_dir)
-        self.assertEqual(
-            ru.scan_rars(),
-            [Path(TEST_DIR, "files/Release-Group/rar_file.rar")],
-        )
-
     def test_unpack_release_dir_rars_no_rars_found(self):
         """Test unpack release dir without RAR files."""
-        tmp_release_search_dir = Path(TEST_DIR, "tmp")
-        tmp_release_search_dir.mkdir()
-
-        ru = ReleaseUnpacker(
-            tmp_release_search_dir, self.tmp_dir, self.unpack_dir
+        release_unpacker = ReleaseUnpacker(
+            self.search_dir, self.tmp_dir, self.unpack_dir
         )
 
-        self.assertFalse(ru.unpack_release_dir_rars())
+        self.assertFalse(release_unpacker.unpack_release_dir_rars())
 
-        # Remove tmp_release_dir
-        shutil.rmtree(tmp_release_search_dir)
+    def test_unpack_releaseses(self):
+        """Test unpack of releases."""
+        for release in ("Release-Group", "Release.with.subs-Group"):
+            self.copy_test_directory_to_search_dir(release)
 
-    def test_unpack_release_dir_rars(self):
-        """Test unpack of release."""
-        tmp_release_search_dir = Path(TEST_DIR, "tmp")
-
-        # Setup tmp_release_search_dir
-        for dir in ("Release-Group", "Release.with.subs-Group"):
-            distutils.dir_util.copy_tree(
-                Path(TEST_DIR, "files", dir), Path(tmp_release_search_dir, dir)
-            )
-
-        ru = ReleaseUnpacker(
-            tmp_release_search_dir, self.tmp_dir, self.unpack_dir
+        release_unpacker = ReleaseUnpacker(
+            self.search_dir, self.tmp_dir, self.unpack_dir
         )
-        ru.unpack_release_dir_rars()
+        release_unpacker.unpack_release_dir_rars()
 
-        # Validate extract of release and subs
+        # Validate extract of releases and subs
         self.assertEqual(
             Path(self.unpack_dir).listdir(),
             [
@@ -185,35 +212,131 @@ class TestReleaseUnpacker(unittest.TestCase):
         # Make sure the unpacked subs rar file is removed
         self.assertFalse(
             Path(
-                tmp_release_search_dir,
-                ("Release.with.subs-Group/Subs/" "subs_inside_rar.rar"),
+                self.search_dir,
+                "Release.with.subs-Group/Subs/subs_inside_rar.rar",
             ).exists()
         )
 
-        # Make sure the relese dirs are removed
+        # Make sure the release dirs are removed
+        self.assertFalse(Path(self.search_dir, "Release-Group").exists())
         self.assertFalse(
-            Path(tmp_release_search_dir, "Release-Group").exists()
-        )
-        self.assertFalse(
-            Path(tmp_release_search_dir, "Release.with.subs-Group").exists()
+            Path(self.search_dir, "Release.with.subs-Group").exists()
         )
 
-        # Remove tmp_release_dir
-        shutil.rmtree(tmp_release_search_dir)
+    def test_unpack_release_with_unpack_time(self):
+        """Test unpack of releases with unpack time mocked for logging."""
+        self.copy_test_directory_to_search_dir("Release-Group")
+
+        release_unpacker = ReleaseUnpacker(
+            self.search_dir, self.tmp_dir, self.unpack_dir
+        )
+
+        with self.assertLogs(self.LOGGER_NAME, level="INFO") as cm:
+            # Mock ago.human() to set a static unpack time
+            with mock.patch(
+                "releaseunpacker.releaseunpacker.human", return_value="1 min"
+            ):
+                release_unpacker.unpack_release_dir_rars()
+
+        self.assertIn(
+            "INFO:releaseunpacker.releaseunpacker:Release-Group.mkv "
+            "unpack done, 1 min",
+            cm.output,
+        )
+
+    def test_unpack_release_with_no_removal(self):
+        """Test unpack of release with no remove activated."""
+        # Unpack releases in search dir without removal
+        for release in ("Release-Group", "Release.with.subs-Group"):
+            self.copy_test_directory_to_search_dir(release)
+
+        release_unpacker = ReleaseUnpacker(
+            self.search_dir, self.tmp_dir, self.unpack_dir, no_remove=True
+        )
+        release_unpacker.unpack_release_dir_rars()
+
+        # Validate extract of releases and subs
+        self.assertEqual(
+            Path(self.unpack_dir).listdir(),
+            [
+                Path(self.unpack_dir, "Release-Group.mkv"),
+                Path(self.unpack_dir, "Release.with.subs-Group.idx"),
+                Path(self.unpack_dir, "Release.with.subs-Group.mkv"),
+                Path(self.unpack_dir, "Release.with.subs-Group.sub"),
+            ],
+        )
+
+        # Make sure the unpacked subs rar file is removed
+        self.assertFalse(
+            Path(
+                self.search_dir,
+                "Release.with.subs-Group/Subs/subs_inside_rar.rar",
+            ).exists()
+        )
+
+        # Make sure the release dirs are not removed
+        self.assertTrue(Path(self.search_dir, "Release-Group").exists())
+        self.assertTrue(
+            Path(self.search_dir, "Release.with.subs-Group").exists()
+        )
+
+    def test_unpack_release_with_already_existing_unpack(self):
+        """Test unpack of release with an existing unpack already."""
+        # Unpack releases in search dir without removal
+        for release in ("Release-Group", "Release.with.subs-Group"):
+            self.copy_test_directory_to_search_dir(release)
+
+        release_unpacker = ReleaseUnpacker(
+            self.search_dir, self.tmp_dir, self.unpack_dir, no_remove=True
+        )
+        release_unpacker.unpack_release_dir_rars()
+
+        # Unpack releases in search dir second time with removal
+        release_unpacker = ReleaseUnpacker(
+            self.search_dir, self.tmp_dir, self.unpack_dir
+        )
+        release_unpacker.unpack_release_dir_rars()
+
+        # Validate extract of releases and subs
+        self.assertEqual(
+            Path(self.unpack_dir).listdir(),
+            [
+                Path(self.unpack_dir, "Release-Group.mkv"),
+                Path(self.unpack_dir, "Release.with.subs-Group.idx"),
+                Path(self.unpack_dir, "Release.with.subs-Group.mkv"),
+                Path(self.unpack_dir, "Release.with.subs-Group.sub"),
+            ],
+        )
+
+        # Make sure the unpacked subs rar file is removed
+        self.assertFalse(
+            Path(
+                self.search_dir,
+                "Release.with.subs-Group/Subs/subs_inside_rar.rar",
+            ).exists()
+        )
+
+        # Make sure the release dirs are removed
+        self.assertFalse(Path(self.search_dir, "Release-Group").exists())
+        self.assertFalse(
+            Path(self.search_dir, "Release.with.subs-Group").exists()
+        )
 
 
-class TestReleaseUnpackerRarFile(unittest.TestCase):
+class TestReleaseUnpackerRarFile(ReleaseUnpackerTestCase, unittest.TestCase):
     """ReleaseUnpackerRarFile test case."""
 
     def test_repr(self):
         """Test object string representation."""
-        rar_file_path = Path(TEST_DIR, "files/Release-Group/rar_file.rar")
-        rurf = ReleaseUnpackerRarFile(rar_file_path)
+        self.copy_test_directory_to_search_dir("Release-Group")
+
+        rar_file = ReleaseUnpackerRarFile(
+            Path(self.search_dir, "Release-Group", "rar_file.rar")
+        )
         self.assertEqual(
-            rurf.__repr__(),
-            (
-                "<ReleaseUnpackerRarFile: {}/files/"
-                "Release-Group/rar_file.rar>".format(TEST_DIR)
+            rar_file.__repr__(),
+            "<ReleaseUnpackerRarFile: {}>".format(
+                Path(self.search_dir, "Release-Group", "rar_file.rar")
             ),
         )
 
@@ -230,12 +353,15 @@ class TestReleaseUnpackerRarFile(unittest.TestCase):
 
     def test_subs_dir(self):
         """Test RAR in Subs dir detected correctly."""
-        rar_file_path = Path(TEST_DIR, "files/Release-Group/rar_file.rar")
-        rurf = ReleaseUnpackerRarFile(rar_file_path)
-        self.assertFalse(rurf.subs_dir)
+        for release in ("Release-Group", "Release.with.subs-Group"):
+            self.copy_test_directory_to_search_dir(release)
+
+        rar_file_path = Path(self.search_dir, "Release-Group", "rar_file.rar")
+        rar_file = ReleaseUnpackerRarFile(rar_file_path)
+        self.assertFalse(rar_file.subs_dir)
 
         rar_file_path = Path(
-            TEST_DIR, ("files/Release.with.subs-Group/Subs/" "subs.rar")
+            self.search_dir, "Release.with.subs-Group", "Subs", "subs.rar"
         )
-        rurf = ReleaseUnpackerRarFile(rar_file_path)
-        self.assertTrue(rurf.subs_dir)
+        rar_file = ReleaseUnpackerRarFile(rar_file_path)
+        self.assertTrue(rar_file.subs_dir)
